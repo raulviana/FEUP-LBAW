dd3595f12750fae18b929daa34c10d112e162fc3
leonormsousa <leonormsousa@sapo.pt>
2020-03-02 12:16:20 +0000

    Added event page

---
+++ event.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Page Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link rel="stylesheet" href="layout.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!--       HEADER      -->
+        <h1 id="event_title">Queen Concert</h1>
+        <!-- image -->
+        <img src="https://www.clarionlbv.com/wp-content/uploads/2014/11/CLBV-Central-Florida-Events-Main-Img.jpg" alt="event_name" width="100%">
+        <div class="container">
+            <div id="event_row" class="row align-items-center">
+                <!-- description -->
+                <p id="event_description" class="col-sm-7">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis dui augue, ut condimentum ante interdum nec. Donec sed magna dolor. Ut consequat pharetra blandit. Etiam convallis eu nisi et rutrum. Suspendisse placerat augue nec rutrum commodo. Mauris vestibulum nisi vel odio pretium, quis vestibulum tellus pellentesque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.</p>
+            
+                <div id="event_info" class="col-sm-5">
+                    <!-- location -->
+                    <h4><a href="https://www.google.com/maps/search/?api=1&query=altice+arena" class="event_location">Altice Arena, Lisbon</a></h3>
+                    <!-- dates -->
+                    <h6>12-12-2020 20:30 GMT - 12-12-2020 23h30 GMT</h6>
+                    <!-- tags -->
+                    <a id="event_tag" class="btn btn-info" href="#" role="button">Music</a>
+                    <a id="event_tag" class="btn btn-info" href="#" role="button">Concert</a>
+                    <a id="event_tag" class="btn btn-info" href="#" role="button">Other tag</a>
+                    <!-- price range -->
+                    <p>12€ - 25€</p>
+                    <!-- link for tickets -->
+                    <a class="btn btn-dark" href="#" role="button">Tickets</a>
+                    <!-- add to whislist -->
+                    <a class="btn btn-dark" href="#" role="button">Wish</a>
+                </div>
+            </div>
+        </div>
+        <!-- organiser info -->
+        <div id="org_info">
+            <img src="https://www.meo.pt/Style%20Library/consumo/images/facebook-meo.png" width="50px" height="50px">
+            <h4>MEO</h4>
+            <p>meo@meo.pt</p>
+            <p>210 210 210</p>
+        </div>
+        <!-- event footer - social media and website-->
+        <div id="event_footer">
+            <a class="btn btn-dark" href="#" role="button">Website</a>
+            <a class="btn btn-dark" href="#" role="button">Facebook</a>
+            <a class="btn btn-dark" href="#" role="button">Instagram</a>
+        </div>
+    </body>
+</html> 
