9120e8526eea8bfc6e37f43c9493ad380f8e1e85
claudiarmamede <up201604832@fe.up.pt>
2020-04-19 19:01:51 +0100

    Removed unnecessary files

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,6 @@
 Route::get('/', 'HomepageController@display')->name('home');
 
 
-// Cards
-Route::get('cards', 'CardController@list');
-Route::get('cards/{id}', 'CardController@show');
-
-// API
-Route::put('api/cards', 'CardController@create');
-Route::delete('api/cards/{card_id}', 'CardController@delete');
-Route::put('api/cards/{card_id}/', 'ItemController@create');
-Route::post('api/item/{id}', 'ItemController@update');
-Route::delete('api/item/{id}', 'ItemController@delete');
 
 // Authentication
 
