db2557183f2861c6e41bc8743f923ac3ee3461d7
claudiarmamede <up201604832@fe.up.pt>
2020-04-26 18:17:21 +0100

    Cleaning profile, fixed posts author

--- resources/views/partials/admin/user_row.blade.php
+++ resources/views/partials/admin/user_row.blade.php
@@ -2,11 +2,11 @@
 <th scope="row">{{$user->id}}</th>
 <td>{{$user->name}}</td>
 <td>{{$user->email}}</td>
-@if(is_null($user->deleted_at))
+<!-- if user->deleted_at is null -->
     <td> <button data-id={{$user->id}} type="button" class="btn btn-danger"> Suspend </button> </td>
     <td data-id={{$user->id}}>Active</td>
-@else 
+<!--else 
     <td> <button data-id={{$user->id}} type="button" class="btn btn-success"> Activate </button> </td>
     <td data-id={{$user->id}}>Suspended</td>
-@endif
+endif -->
 <tr>
