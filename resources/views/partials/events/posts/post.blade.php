beb3314e31bb0a6bb4d2ddf7df85f85fc725c526
Raul Viana <raulmanuelviana@gmail.com>
2020-05-03 11:23:12 +0100

    posts from view now display user photo

--- resources/views/partials/events/posts/post.blade.php
+++ resources/views/partials/events/posts/post.blade.php
@@ -2,8 +2,8 @@
 
 <article class="post p-3 mb-3">
     <div style="margin-bottom: 1.5rem;" class="d-flex flex-row align-items-center">
-        <img src="https://pbs.twimg.com/profile_images/973548356462051329/PldBA7ID_400x400.jpg" class="rounded-circle mr-2" alt="Owner" width="50px">
-     
+        <img src={{ Storage::url('users/'.$post['writer']['photo'])}} class="rounded-circle mr-2" alt="Owner" width="50px">
+    
             @if(is_null($post['writer']['name']))
                 <h6> <b> SUSPENDED USER </b> says <h6>
             @else 
